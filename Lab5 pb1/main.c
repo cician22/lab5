1b353cc9111ddef9d15615337ffd24ca5dc62f64
cician22 <dancristian12345@gmail.com>
2023-03-29 12:37:24 +0300

    pb1 lab 5

---
+++ Lab5 pb1/main.c
@@ -0,0 +1,33 @@
+#include "stive.h"
+
+typedef char Data;
+
+int main()
+{
+    int i = 0;
+    Node * stackTop = NULL ;
+    char v[] = "abcd";
+    for ( i = 0; i < 4; i++){
+        push(&stackTop, v[i]);
+    }
+    while(i != 0){
+        printf("%c ", top(stackTop));
+        pop(&stackTop);
+        i--;
+    }
+    printf("\n");
+    for ( i = 0; i < 4; i++){
+        push(&stackTop, v[i]);
+    }
+    push(&stackTop, 'Q');
+    i = 5;
+    while(i != 0){
+        printf("%c ", top(stackTop));
+        pop(&stackTop);
+        i--;
+    }
+    //pop(&stackTop);
+    //isEmpty(stackTop);
+    //deleteStack(&stackTop);
+    return 0;
+}
